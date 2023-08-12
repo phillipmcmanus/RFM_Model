bc435f3c8ac3b59b625b3f11202c283bd117c518
phillipmcmanus <phillipmcmanus14@gmail.com>
2023-08-11 21:03:35 -0500

    Some visualizations

--- RFM_Model.R
+++ RFM_Model.R
@@ -1,17 +1,24 @@
 #INFO 523 Data Mining
-#Auther: Phillip McManus
+#Professor: Cristian Román-Palacios
+#University of Arizona
+
+#Author: Phillip McManus
 #RFM Segmentation Model
 
 
 library(dplyr)
+library("ggplot2")
 
+#Read in a sample dataset of store transactions
+#This sample only contains the fields we need, Customer ID, Order ID, Order Date and Order Amount (Sales)
 transactions <- read.csv('data/store_transactions.csv')
 
 summary(transactions)
 head(transactions)
-head(rfm_data)
+head(rfm_scores)
 
-
+#Remove any records that have NA in the required fields
+transactions <- na.omit(transactions)
 
 #Transform the text date in the dataset to an actual date format
 transactions$Order.Date <- as.Date(transactions$Order.Date, format = '%m/%d/%Y')
@@ -41,3 +48,27 @@ rfm_data$frequency_score <- cut(rfm_data$frequency,
 rfm_data$monetary_score <- cut(rfm_data$monetary, 
                                breaks=c(quantile(rfm_data$monetary, probs = seq(0, 1, by = 0.20))), 
                                labels=c(1,2,3,4,5))
+
+#Convert scores to numeric values
+rfm_data$recency_score <- as.numeric(rfm_data$recency_score)
+rfm_data$frequency_score <- as.numeric(rfm_data$frequency_score)
+rfm_data$monetary_score <- as.numeric(rfm_data$monetary_score)
+
+
+ggplot(rfm_data, aes(recency_score, frequency_score)) +
+  geom_jitter(aes(colour = monetary_score))  
+
+ggplot(rfm_data, aes(recency, frequency)) +
+  geom_jitter(aes(colour = monetary_score), size = 0.5)  
+
+ggplot(rfm_data, aes(recency_score, frequency_score)) +
+  geom_tile(aes(fill = monetary_score)) +
+  scale_fill_gradient(low = "white", high = "navy")  
+
+barplot(rfm_data$recency_score)
+
+
+#Save RFM Scores to a separate data frame
+rfm_scores <- rfm_data[,c("recency_score", "frequency_score", "monetary_score")]
+
+
