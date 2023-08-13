71e4a373d9ba8b33616d117015972e65dd7db66e
phillipmcmanus <phillipmcmanus14@gmail.com>
2023-08-13 15:22:42 -0500

    Working code to complete the model

--- RFM_Model.R
+++ RFM_Model.R
@@ -8,14 +8,11 @@
 
 library(dplyr)
 library("ggplot2")
+library(factoextra)
 
 #Read in a sample dataset of store transactions
 #This sample only contains the fields we need, Customer ID, Order ID, Order Date and Order Amount (Sales)
 transactions <- read.csv('data/store_transactions.csv')
-
-summary(transactions)
-head(transactions)
-head(rfm_scores)
 
 #Remove any records that have NA in the required fields
 transactions <- na.omit(transactions)
@@ -49,14 +46,20 @@ rfm_data$monetary_score <- cut(rfm_data$monetary,
                                breaks=c(quantile(rfm_data$monetary, probs = seq(0, 1, by = 0.20))), 
                                labels=c(1,2,3,4,5))
 
+#If any of the metrics were too low and caused an NA from the quantile split, assign the lowest tier '1' to them
+rfm_data$recency_score[is.na(rfm_data$recency_score)] <- 1
+rfm_data$frequency_score[is.na(rfm_data$frequency_score)] <- 1
+rfm_data$monetary_score[is.na(rfm_data$monetary_score)] <- 1
+
 #Convert scores to numeric values
 rfm_data$recency_score <- as.numeric(rfm_data$recency_score)
 rfm_data$frequency_score <- as.numeric(rfm_data$frequency_score)
 rfm_data$monetary_score <- as.numeric(rfm_data$monetary_score)
 
 
+
 ggplot(rfm_data, aes(recency_score, frequency_score)) +
-  geom_jitter(aes(colour = monetary_score))  
+  geom_jitter(aes(colour = monetary_score), width = 0.25, height = 0.25)  
 
 ggplot(rfm_data, aes(recency, frequency)) +
   geom_jitter(aes(colour = monetary_score), size = 0.5)  
@@ -65,10 +68,40 @@ ggplot(rfm_data, aes(recency_score, frequency_score)) +
   geom_tile(aes(fill = monetary_score)) +
   scale_fill_gradient(low = "white", high = "navy")  
 
-barplot(rfm_data$recency_score)
-
 
 #Save RFM Scores to a separate data frame
 rfm_scores <- rfm_data[,c("recency_score", "frequency_score", "monetary_score")]
+
+#Plot the optimal number of clusters
+fviz_nbclust(rfm_scores, kmeans, method = "wss")  #The result of this plot shows the optimal number of clusters being 5 for this dataset
+
+#Run K-Mean Clustering
+set.seed(314)
+rfm_groups <- kmeans(rfm_scores, centers = 5, nstart = 25)
+
+#Plot the distribution of the clusters
+fviz_cluster(rfm_groups, geom = "point", data = rfm_scores)
+
+#Create a new data frame using the Centers data from the kmeans function
+rfm_centers <- data.frame(rfm_groups$centers)
+
+#Plot the scores for each cluster
+ggplot(rfm_centers, aes(recency_score, frequency_score, size = monetary_score)) +
+  geom_point() + scale_size(range = c(1, 15)) 
+
+#Add the cluster numbers to the original dataset
+rfm_data <- cbind(rfm_data, "segment" = rfm_groups$cluster)
+
+#Summarise the mean of each metrics for each cluster
+rfm_segments <- rfm_data %>% group_by(segment) %>%
+  summarise(avg_recency = round(mean(recency)),
+            avg_frequency = round(mean(frequency)),
+            avg_monetary = round(mean(monetary)))
+
+#Display the table of segment averages and the visual plot of them
+rfm_segments
+ggplot(rfm_segments, aes(avg_recency, avg_frequency, size = avg_monetary)) +
+  geom_point() + scale_size(range = c(1, 15))
+
 
 
